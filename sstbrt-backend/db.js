3ba2e031b8e3bbb859721ca64becab9a18d9b77b
Obed-Siervo <169004065+Obed-Siervo@users.noreply.github.com>
2026-05-06 10:22:21 -0400

    Update db.js

--- sstbrt-backend/db.js
+++ sstbrt-backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'admin123',
-  database: process.env.DB_NAME || 'sstbrt_db',
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
